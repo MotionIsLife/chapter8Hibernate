f3849284868a3264b2e594936abcd36c73c59d83
Iuliia <mbeq9665>
2018-02-04 00:55:40 +0300

    Initial commit

---
+++ src/main/java/motionIsLife/vo/Hobby.java
@@ -0,0 +1,23 @@
+package motionIsLife.vo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "hobby")
+public class Hobby implements Serializable {
+    private String hobbyId;
+
+    @Id
+    @Column(name = "HOBBY_ID")
+    public String getHobbyId() {
+        return hobbyId;
+    }
+
+    public void setHobbyId(String hobbyId) {
+        this.hobbyId = hobbyId;
+    }
+}
