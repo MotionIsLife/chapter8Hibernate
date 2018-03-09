316e689b608b395037b7677b007da492a00c065d
kprikhodko <1_v_pole@inbox.ru>
2018-03-09 22:39:07 +0300

    Добавлено обновление информации о возрасте машины

--- src/main/java/motionIsLife/App.java
+++ src/main/java/motionIsLife/App.java
@@ -1,20 +1,14 @@
 package motionIsLife;
 
-import motionIsLife.dao.CarService;
-import motionIsLife.vo.Car;
 import org.springframework.context.support.GenericXmlApplicationContext;
-
-import java.util.*;
 
 public class App {
     public static void main(String[] args) {
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        ctx.load("classpath:app-context-annotation.xml");
+        ctx.load("classpath:task-job-app-context.xml");
         ctx.refresh();
-        CarService carService = ctx.getBean("carService", CarService.class);
-        List<Car> cars = carService.findAll();
-        for (Car car : cars) {
-            System.out.println(car);
+        while(true) {
+
         }
     }
 }
